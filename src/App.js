8e04a825322b4e4152ee078773f7f36852f30813
pavanb0 <pavanbagwe16@gmail.com>
2023-12-24 23:36:47 +0530

    added new changes in the main section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Hero from './assets/Hero.jpg'
+import Projects from './Projects';
 
 function App() {
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    element.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section className='hero-header'>
+        <h1>Hello i am Pavan Bagwe</h1>
+        <p>passionate about building software </p>
+        </section>
+     
+        <nav>
+            <button onClick={() => scrollToSection('projects')}>Projects</button>
+            <button onClick={() => scrollToSection('education')}>Education</button>
+            <button onClick={() => scrollToSection('skills')}>Skills</button>
+            <button onClick={() => scrollToSection('contact')}>Contact</button>
+        </nav>
+       
+        <img src={Hero} alt='hero'/>
       </header>
+
+     
+     <Projects/>
+
+
+
+      <section id="education">
+        <h2>Education</h2>
+        <p>Your Degree, University, Graduation Year</p>
+        
+      </section>
+
+      <section id="skills">
+        <h2>Skills</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+        </ul>
+      </section>
+
+      <section id="contact">
+        <h2>Contact</h2>
+        <form>
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Your Email" />
+          <textarea placeholder="Your Message"></textarea>
+          <button type="submit">Send Message</button>
+        </form>
+      </section>
     </div>
   );
 }
---
+++ src/Projects.js
@@ -0,0 +1,17 @@
+function Projects(){
+    return(
+        <section id="projects">
+        <h2>Projects</h2>
+        <div className="project">
+          <img src="project1.jpg" alt="Project 1" />
+          <p>Project 1 Description</p>
+        </div>
+        <div className="project">
+          <img src="project2.jpg" alt="Project 2" />
+          <p>Project 2 Description</p>
+        </div>
+        
+      </section>
+    )
+}
+export default Projects;
